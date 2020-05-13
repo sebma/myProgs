3a2e95cfb5bd06e73e4ed841062241b2470ef56a
sebma <sebma@users.noreply.github.com>
2020-05-13 13:00:47 +0200

    Updated sizeof.c

--- c/sizeof.c
+++ c/sizeof.c
@@ -1,42 +1,41 @@
 #include <stdio.h>
 #include <stdint.h> //uint8_t, etc...
 
-int len_uint8_t(uint8_t *x) {
-//	printf("=> __func__ = %s\n", __func__ );
-	return (int)(sizeof(x) / sizeof(uint8_t));
-}
-
 int main (int argc, char **argv) {
 
-	printf("sizeof(char):   %d bits\n", 8 * (int) sizeof(char));
+	printf("\nsizeof(char):  %lu bits\n", 8 * sizeof(char) );
 
-	printf("sizeof(short): %d bits\n", 8 * (int) sizeof(short));
+	printf("sizeof(short): %lu bits\n", 8 * sizeof(short));
 
-	printf("sizeof(int):   %d bits\n", 8 * (int) sizeof(int));
+	printf("sizeof(int):   %lu bits\n", 8 * sizeof(int));
 
-	printf("sizeof(long):  %d bits\n", 8 * (int) sizeof(long));
+	printf("sizeof(long):  %lu bits\n", 8 * sizeof(long));
 
-	printf("sizeof(void *):%d bits\n", 8 * (int) sizeof(void *));
+	printf("sizeof(long long): %lu bits\n", 8 * sizeof(long long));
 
-	printf("sizeof(long long): %d bits\n", 8 * (int) sizeof(long long));
+	printf("sizeof(void *):%lu bits\n", 8 * sizeof(void *));
 
-	printf("=> Standard types from stdint.h :\n");
+	printf("sizeof(float):  %lu bits\n", 8 * sizeof(float));
 
-	printf("sizeof(uint8_t): %d bits\n", 8 * (int) sizeof(uint8_t));
+	printf("sizeof(double):  %lu bits\n", 8 * sizeof(double));
 
-	printf("sizeof(int16_t): %d bits\n", 8 * (int) sizeof(int16_t));
+	printf("sizeof(long double):  %lu bits\n", 8 * sizeof(long double));
 
-	printf("sizeof(int32_t): %d bits\n", 8 * (int) sizeof(int32_t));
+	printf("\n=> Standard types from stdint.h :\n");
 
-	printf("sizeof(int64_t): %d bits\n", 8 * (int) sizeof(int64_t));
+	printf("sizeof(int8_t): %lu bits\n", 8 * sizeof(int8_t));
 
-	uint8_t a[15];
+	printf("sizeof(int16_t): %lu bits\n", 8 * sizeof(int16_t));
 
-	printf("=> sizeof(a) = %lu\n", sizeof(a));
+	printf("sizeof(int32_t): %lu bits\n", 8 * sizeof(int32_t));
 
-	printf("=> sizeof(a)/sizeof(int) = %lu\n", sizeof(a)/sizeof(uint8_t));
+	printf("sizeof(int64_t): %lu bits\n", 8 * sizeof(int64_t));
 
-	printf("=> len_uint8_t = %d\n", len_uint8_t(a));
+	uint8_t a[15]; //byte array
+
+	printf("\n=> sizeof(a) = %lu\n", sizeof(a));
+
+	printf("=> sizeof(a)/sizeof(uint8_t) = %lu\n", sizeof(a)/sizeof(uint8_t));
 
 //  printf("Hit enter to exit.\n");
 //  char *scannedText;
